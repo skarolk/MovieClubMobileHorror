4eb80426c39ddf324398d3f30c44fe93b0200023
skarolk <skarolkiewicz@gmail.com>
2019-02-20 16:08:23 -0500

    first commit

---
+++ navigation/MainTabNavigator.js
@@ -0,0 +1,86 @@
+import React from "react";
+import { Platform } from "react-native";
+import {
+  createStackNavigator,
+  createBottomTabNavigator,
+  createMaterialTopTabNavigator
+} from "react-navigation";
+
+import TabBarIcon from "../components/TabBarIcon";
+import HomeScreen from "../screens/HomeScreen";
+import LinksScreen from "../screens/LinksScreen";
+import SettingsScreen from "../screens/SettingsScreen";
+
+const HomeStack = createStackNavigator({
+  Home: HomeScreen
+});
+
+HomeStack.navigationOptions = {
+  tabBarLabel: "Home",
+  tabBarOptions: {
+    labelStyle: {
+      marginTop: 25
+    }
+  },
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === "ios"
+          ? `ios-information-circle${focused ? "" : "-outline"}`
+          : "md-information-circle"
+      }
+    />
+  )
+};
+
+const LinksStack = createStackNavigator({
+  Links: LinksScreen
+});
+
+LinksStack.navigationOptions = {
+  tabBarLabel: "My Club",
+  tabBarOptions: {
+    labelStyle: {
+      marginTop: 25
+    }
+  },
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === "ios" ? "ios-link" : "md-link"}
+    />
+  )
+};
+
+const SettingsStack = createStackNavigator({
+  Settings: SettingsScreen
+});
+
+SettingsStack.navigationOptions = {
+  tabBarLabel: "Ratings",
+  tabBarOptions: {
+    labelStyle: {
+      marginTop: 25
+    }
+  },
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === "ios" ? "ios-options" : "md-options"}
+    />
+  )
+};
+
+// render navbar on top
+export default createMaterialTopTabNavigator({
+  HomeStack,
+  LinksStack,
+  SettingsStack
+});
+// render navbar on bottom
+// export default createBottomTabNavigator({
+//   HomeStack,
+//   LinksStack,
+//   SettingsStack
+// });
