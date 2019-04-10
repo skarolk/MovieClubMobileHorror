0d3092a7be411e6e8701f4c4ddccf3bd95310d62
skarolk <skarolkiewicz@gmail.com>
2019-04-10 16:14:11 -0400

    cleaning up code

--- App.js
+++ App.js
@@ -36,15 +36,15 @@ export default class App extends React.Component {
   _loadResourcesAsync = async () => {
     return Promise.all([
       Asset.loadAsync([
-        require("./assets/images/robot-dev.png"),
-        require("./assets/images/robot-prod.png")
+        require("./assets/images/smc_app_icon.jpg"),
+        require("./assets/images/smc_logo_final.png"),
+        require("./assets/images/smclub_background.png"),
+        require("./assets/images/smlottery_background.png"),
+        require("./assets/images/smratings_background.png")
       ]),
       Font.loadAsync({
         // This is the font that we are using for our tab bar
-        ...Icon.Ionicons.font,
-        // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this if you are not using it in your app
-        "space-mono": require("./assets/fonts/SpaceMono-Regular.ttf")
+        ...Icon.Ionicons.font
       })
     ]);
   };
